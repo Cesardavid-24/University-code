f081b39b783ec4b7319dfb1f2b396ceb6a300af3
cesar <norsciacesar@gmail.com>
2022-10-23 14:06:19 -0400

    data stucture

--- Pila.cpp
+++ Pila.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
-struct Nodo {
+struct Nodo{
     int valor;
     Nodo *siguiente;
 };
-void insertar_dato(Nodo *pila , int valor){
-    //crear el espacio en memoria   
-    Nodo *nodo_nuevo = new Nodo();
-    //insertar el valor dentro del nodo
-    nodo_nuevo->valor = valor;
-    //cargar el puntero al siguiente nodo
-    nodo_nuevo->siguiente = pila;
-    //asignar el nuevo nodo a la pila
-    pila = nodo_nuevo;
-    std::cout<<"Dato insetado correctamente."<<std::endl;
+void mostrarNodo(Nodo *&pila){
+  Nodo *nodo_auxiliar = pila;
+  if (nodo_auxiliar != NULL){
+    while (nodo_auxiliar != NULL){
+      std::cout<<nodo_auxiliar->valor << std::endl;
+      nodo_auxiliar = nodo_auxiliar->siguiente;
+    }
+    delete nodo_auxiliar;
+    }
+    else{
+      std::cout<<"La pila esta vacia"<<std::endl;
+    }
 }
-int main () {
-    int numero = 5; 
+void crearNodo(Nodo *&pila, int dato){
+    Nodo *nodo_nuevo = new Nodo();
+    nodo_nuevo->valor = dato;
+    nodo_nuevo->siguiente = pila;
+    pila = nodo_nuevo;
+    std::cout<<"Dato insetado"<<std::endl;
+}
+void eliminarNodo(Nodo *&pila, int &dato){
+    Nodo *nodo_auxiliar = pila;
+    dato = nodo_auxiliar->valor;
+    pila = nodo_auxiliar->siguiente;
+    delete nodo_auxiliar;
+   
+}
+int main(){
     Nodo *pila = NULL;
-
-    insertar_dato(pila, numero);
-
-    return 0;
+    int dato;
+    crearNodo(pila, 5);
+    crearNodo(pila, 6);
+    crearNodo(pila, 7);
+    mostrarNodo(pila);         
+    while (pila != NULL){
+        eliminarNodo(pila,dato);
+        if(pila != NULL){
+            std::cout<<dato<<" <- ";
+        } else{
+            std::cout<<dato<<".";
+        }
+    }    
+    system("pause");        
+    return EXIT_SUCCESS;
 }
